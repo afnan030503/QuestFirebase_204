62d999a9b3b44cc64aa7fe20173c169971f7d32d
afnan030503 <muhammadafnanmaulidi@gmail.com>
2025-01-12 20:47:14 +0700

    menambahkan fungsi get all sama insert mhs

--- app/src/main/java/com/example/androidfirebase/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/androidfirebase/repository/MahasiswaRepository.kt
@@ -1,7 +1,9 @@
 package com.example.androidfirebase.repository
 
 import com.example.androidfirebase.model.Mahasiswa
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.tasks.await
 
 interface MahasiswaRepository {
     suspend fun  getMahasiswa(): Flow<List<Mahasiswa>>
