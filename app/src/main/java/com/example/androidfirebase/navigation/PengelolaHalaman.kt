6902758585a84a555fcbd142cacdedc6bb22625a
afnan030503 <muhammadafnanmaulidi@gmail.com>
2025-01-13 14:55:40 +0700

    menambah fun PengelolaHalaman.kt dan mengimpor insertmahasiswa view dan home view di home view

---
+++ app/src/main/java/com/example/androidfirebase/navigation/PengelolaHalaman.kt
@@ -0,0 +1,38 @@
+package com.example.androidfirebase.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.androidfirebase.view.HomeView
+import com.example.androidfirebase.view.InsertMahasiswaView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeView(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertMahasiswaView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
